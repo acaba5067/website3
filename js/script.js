e3eb5daab09d6259ef2e5498633642a8d3246122
Ibrahim Elshary <acaba5067@gmail.com>
2024-05-13 23:21:12 +0200

    up

--- js/script.js
+++ js/script.js
@@ -3,3 +3,27 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+
+document.getElementById("btn").onclick=function(){
+  document.getElementById("copy").style.display="block"
+  
+}
+document.getElementById("btn").onmouseover=function(){
+  document.getElementById("copy").style.display="none"
+}
+
+document.getElementById("btn2").onclick=function(){
+  document.getElementById("copy2").style.display="block"
+  
+}
+document.getElementById("btn2").onmouseover=function(){
+  document.getElementById("copy2").style.display="none"
+}
+
+document.getElementById("btn3").onclick=function(){
+  document.getElementById("copy3").style.display="block"
+  
+}
+document.getElementById("btn3").onmouseover=function(){
+  document.getElementById("copy3").style.display="none"
+}
